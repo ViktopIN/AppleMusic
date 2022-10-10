ca246ceffed54c62177726628bd336e99b7c16c1
Viktor <viktopiniaginya@gmail.com>
2022-10-10 19:56:04 +0300

    add playlist pictures

---
+++ AppleMusic/Sources/Scenes/SearchScreen/SearchBarScreen/Model/Playlist model.swift
@@ -0,0 +1,12 @@
+//
+//  Playlist model.swift
+//  AppleMusic
+//
+//  Created by Виктор on 10.10.2022.
+//
+
+import Foundation
+
+struct PlaylistModel {
+    static let 
+}
---
+++ AppleMusic/Sources/Scenes/SearchScreen/SearchBarScreen/View/Playlist/PlaylistView.swift
@@ -0,0 +1,20 @@
+//
+//  PlaylistView.swift
+//  AppleMusic
+//
+//  Created by Виктор on 10.10.2022.
+//
+
+import SwiftUI
+
+struct PlaylistView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlaylistView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlaylistView()
+    }
+}
